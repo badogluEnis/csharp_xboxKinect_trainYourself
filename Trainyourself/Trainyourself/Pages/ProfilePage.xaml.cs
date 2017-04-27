c406c6fc4f6feeab91a8559ceb78892b6f2e8baf
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-04-27 09:42:19 +0200

    worked on Gui from ProfilePage

--- Trainyourself/Trainyourself/Pages/ProfilePage.xaml.cs
+++ Trainyourself/Trainyourself/Pages/ProfilePage.xaml.cs
@@ -1,42 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Model;
+﻿using Model;
 
 namespace Trainyourself.Pages
 {
     /// <summary>
     /// Interaction logic for ProfilePage.xaml
     /// </summary>
-    public partial class ProfilePage : Page
+    public partial class ProfilePage
     {
         public ProfilePage()
         {
             InitializeComponent();
         }
 
-        public string name { get; set; }
-        public string lastname { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
 
         public string GetName(User user)
         {
-            return name = user.Name;
+            return Name = user.Name;
         }
 
         public string GetLastname(User user)
         {
-            return lastname = user.Lastname;
+            return Lastname = user.Lastname;
         }
 
         public void GetHeight(User user)
