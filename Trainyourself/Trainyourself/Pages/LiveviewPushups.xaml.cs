e71cfc940b363b26d88524ec40c9415103204a51
xahanix <altin.9@hotmail.ch>
2017-05-11 15:20:17 +0200

    Calibration added

--- Trainyourself/Trainyourself/Pages/LiveviewPushups.xaml.cs
+++ Trainyourself/Trainyourself/Pages/LiveviewPushups.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Timers;
 using System.Windows;
 using System.Windows.Forms;
@@ -19,6 +20,14 @@ namespace Trainyourself.Pages
         public float ShoulderRightX;
         public float ShoulderRightY;
 
+        public float CalibrateLeftShoulderTopY;
+        public float CalibrateRightShoulderTopY;
+        public float CalibrateRightShoulderBottomY;
+        public float CalibrateLeftShoulderBottomY;
+
+        public int counter;
+        public bool WarUnten = false;
+
         private KinectProvider _kinectProvider = new KinectProvider();
 
         System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
@@ -27,10 +36,12 @@ namespace Trainyourself.Pages
         {
             InitializeComponent();
             Image.Source = _kinectProvider._colorBitmap;
-            timer.Interval = 5000;
+            timer.Interval = 3000;
             timer.Start();
             timer.Tick += TimerOnTick;
             _kinectProvider.PositionChanged += SkeletonChanged;
+
+      
         }
 
         private void TimerOnTick(object sender, EventArgs eventArgs)
@@ -45,7 +56,9 @@ namespace Trainyourself.Pages
             ShoulderLeftY = skeleton.Joints[JointType.ShoulderLeft].Position.Y;
             ShoulderRightX = skeleton.Joints[JointType.ShoulderRight].Position.X;
             ShoulderRightY = skeleton.Joints[JointType.ShoulderRight].Position.Y;
-
+           
+            CheckCount();
+            CheckUp();
         }
 
         private void QuitButton_OnClick(object sender, RoutedEventArgs e)
@@ -54,5 +67,38 @@ namespace Trainyourself.Pages
             _kinectProvider.Stop();
             timer.Stop();
         }
+
+        public void CheckCount()
+        {
+            if (ShoulderRightY < 0.3 && ShoulderLeftY < 0.3 && !WarUnten)
+            {
+                Debug.WriteLine("Peer");
+                counter = counter + 1;
+                Currentrun.Content = $"Current Run: {counter}";
+                WarUnten = true;
+            }
+        }
+
+        public void CheckUp()
+        {
+            if (ShoulderRightY > 0.4 && ShoulderLeftY > 0.4)
+            {
+                Debug.WriteLine("Mueter");
+                WarUnten = false;
+            }
+        }
+
+        public void CalibrateTop(Skeleton skeleton)
+        {
+            CalibrateLeftShoulderTopY = skeleton.Joints[JointType.ShoulderLeft].Position.Y;
+            CalibrateRightShoulderTopY = skeleton.Joints[JointType.ShoulderRight].Position.Y;
+        }
+
+        public void CalibrateBottom(Skeleton skeleton)
+        {
+            CalibrateLeftShoulderBottomY = skeleton.Joints[JointType.ShoulderLeft].Position.Y;
+            CalibrateRightShoulderBottomY = skeleton.Joints[JointType.ShoulderRight].Position.Y;
+        }
+
     }
 }
