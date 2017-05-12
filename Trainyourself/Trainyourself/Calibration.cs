7335908fbadcf8bb8ec1a8f8883b620408a655f3
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-05-12 15:21:59 +0200

    finished Calibration

--- Trainyourself/Trainyourself/Calibration.cs
+++ Trainyourself/Trainyourself/Calibration.cs
@@ -1,53 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media.Animation;
-using Microsoft.Kinect;
-using KinectConnection;
+﻿using Microsoft.Kinect;
 
 namespace Trainyourself
 {
     class Calibration
     {
-        private float[] ShoulderRightY = new float[401];
-        private float[] ShoulderLeftY = new float[401];
-        private float[] HandRightY = new float[401];
-        private float[] HandLeftY = new float[401];
+        private const int ARRAY_LENGTH = 101;
+        private const float TOLLERANCE = 0.2f;
+        private const int ARRAY_LENGHT_HALF = 75;
+        private float[] ShoulderRightY = new float[ARRAY_LENGTH];
+        private float[] ShoulderLeftY = new float[ARRAY_LENGTH];
+        private float[] HandRightY = new float[ARRAY_LENGTH];
+        private float[] HandLeftY = new float[ARRAY_LENGTH];
         private int i;
-        private int j;
-        public bool IsNotMoving(Skeleton skeleton)
+
+       
+
+        public float ShoulderHandDistanceRight { get; private set; }
+        public float ShoulderHandDistanceLeft { get; private set; }
+        public bool IsCalibrated { get; private set; }
+
+        
+
+        public void Calibrate(Skeleton skeleton)
         {
-            int b = j + 1;
-            if (ShoulderRightY[j] - ShoulderRightY[b] < 0.03 && ShoulderRightY[i] - ShoulderRightY[b] > -0.03)
+            if (IsCalibrated == false)
             {
-                if (ShoulderLeftY[j] - ShoulderLeftY[b] < 0.03 && ShoulderLeftY[i] - ShoulderLeftY[b] > -0.03)
+                if (i < ARRAY_LENGTH - 1)
                 {
-                    if (HandRightY[j] - HandRightY[b] < 0.03 && HandRightY[i] - HandRightY[b] > -0.03)
+                    FillArray(skeleton);
+                }
+                else
+                {
+                    if (CheckArraysForMoves() != true)
                     {
-                        if (HandLeftY[j] - HandLeftY[b] < 0.03 && HandLeftY[i] - HandLeftY[b] > -0.03)
-                        {
-                            j++;
-                            return true;
-                        }
+                        CalculateShoulderHandDistance();
+                        IsCalibrated = true;
                     }
+
+                    ClearArray();
+                }
+            }
+            
+        }
+
+        private void CalculateShoulderHandDistance()
+        {
+            ShoulderHandDistanceRight = ShoulderRightY[ARRAY_LENGHT_HALF] - HandRightY[ARRAY_LENGHT_HALF];
+            ShoulderHandDistanceLeft = ShoulderLeftY[ARRAY_LENGHT_HALF] - HandLeftY[ARRAY_LENGHT_HALF];
+        }
+
+        private bool CheckArraysForMoves()
+        {
+
+            float referenceShoulderRight = ShoulderRightY[0];
+            float referenceShoulderLeft = ShoulderLeftY[0];
+            float referenceHandRight = HandRightY[0];
+            float referenceHandLeft = HandLeftY[0];
+            for (int i = 0; i < ARRAY_LENGTH; i++)
+            {
+                if (!(ShoulderRightY[i] < referenceShoulderRight + TOLLERANCE && ShoulderRightY[i] > referenceShoulderRight - TOLLERANCE))
+                {
+                    return true;
+                }
+                if (!(ShoulderLeftY[i] < referenceShoulderLeft + TOLLERANCE && ShoulderRightY[i] > referenceShoulderLeft - TOLLERANCE))
+                {
+                    return true;
+                }
+                if (!(HandRightY[i] < referenceHandRight + TOLLERANCE && ShoulderRightY[i] > referenceHandRight - TOLLERANCE))
+                {
+                    return true;
+                }
+                if (!(HandRightY[i] < referenceHandLeft + TOLLERANCE && ShoulderRightY[i] > referenceHandLeft - TOLLERANCE))
+                {
+                    return true;
                 }
             }
             return false;
         }
 
-        public void FillArray(Skeleton skeleton)
+        private void FillArray(Skeleton skeleton)
         {
-            if (i < 400)
-            {
                 ShoulderRightY[i] = skeleton.Joints[JointType.ShoulderRight].Position.Y;
                 ShoulderLeftY[i] = skeleton.Joints[JointType.ShoulderLeft].Position.Y;
                 HandRightY[i] = skeleton.Joints[JointType.HandRight].Position.Y;
                 HandLeftY[i] = skeleton.Joints[JointType.HandLeft].Position.Y;
                 i++;
-            }
+        }
+
+        private void ClearArray()
+        {
+            ShoulderRightY = new float[ARRAY_LENGTH];
+            ShoulderLeftY = new float[ARRAY_LENGTH];
+            HandRightY = new float[ARRAY_LENGTH];
+            HandLeftY = new float[ARRAY_LENGTH];
+            i = 0;
         }
     }
 }
